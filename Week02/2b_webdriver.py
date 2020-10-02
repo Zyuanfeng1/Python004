a3c014d452fe6f30ec6a9570c8f75b74ac40ee29
yuanfeng <929809235@qq.com>
2020-10-03 01:52:56 +0800

    noe yet

---
+++ Week02/2b_webdriver.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import time
+
+try:
+    browser = webdriver.Chrome()
+    # 需要安装chrome driver, 和浏览器版本保持一致
+    # http://chromedriver.storage.googleapis.com/index.html
+    
+    browser.get('https://shimo.im/login?from=home')
+    time.sleep(1)
+
+    # browser.switch_to.frame(browser.find_elements_by_tag_name('iframe')[0])
+    # btm1 = browser.find_element_by_xpath('/html/body/div[1]/div[1]/ul[1]/li[2]')
+    # btm1.click()
+
+    browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[1]/div/input').send_keys('13470188924')
+    browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[2]/div/input').send_keys('zyfzxh')
+    time.sleep(1)
+    browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/button').click()
+
+    #cookies = browser.get_cookies() # 获取cookies
+    #print(cookies)
+    time.sleep(3)
+
+except Exception as e:
+    print(e)
+finally:    
+    pass
+    
