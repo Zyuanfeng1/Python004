7f6ea2b060ad482963502ffa282ad3f2e40f019c
yuanfeng <929809235@qq.com>
2020-10-03 01:32:59 +0800

    noe yet

---
+++ Week02/spiders/spiders/pipelines.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import pymysql
+
+class SpidersPipeline:
+    def process_item(self, item, spider):
+        name = item['name']
+        #item['name']="".join(item['name'].split())
+        category = item['category']
+        plan_date = item['plan_date']
+
+
+        conn = pymysql.connect(host='localhost',
+                               port=3306,
+                               user='root',
+                               password='',
+                               database='test',
+                               charset='utf8mb4'
+                               )
+
+        # 获得cursor游标对象
+        con1 = conn.cursor()
+
+        # 操作的行数
+        count = con1.execute('insert into movies values(%s,%s,%s);',(name,category,plan_date))
+        count = con1.execute('commit;')
+
+
+        # 获得所有查询结果
+        print(con1.fetchall())
+
+        con1.close()
+        conn.close()
+
+        # 执行批量插入
+        # values = [(id,'testuser'+str(id)) for id in range(4, 21) ]
+        # cursor.executemany('INSERT INTO '+ TABLE_NAME +' values(%s,%s)' ,values)
+
+        # output = f'|{name}|\t|{category}|\t|{plan_date}|\n\n'
+        # with open('./maoyan2.csv', 'a', encoding='utf-8') as article:
+        #     article.write(output)
+        return item
---
+++ Week02/spiders/spiders/spiders/maoyan.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.selector import Selector
+
+from spiders.items import SpidersItem
+
+
+class MaoyanSpider(scrapy.Spider):
+    name = 'maoyan'
+    allowed_domains = ['maoyan.com']
+    start_urls = ['https://maoyan.com/films?showType=3']
+
+    def parse(self, response):
+        print(response.url)
+        movies=Selector(response=response).xpath('//div[@class="movie-hover-info"]')
+
+        for movie in movies[:10] :
+            name=movie.xpath('./div[1]/span[@class="name "]/text()')
+            item = SpidersItem()
+            item['name']=name.extract_first().strip()
+            print(name.extract_first().strip())
+            category=movie.xpath('./div[2]/text()')
+            item['category']=category.extract()[1].strip()
+            print(category.extract()[1].strip())
+            plan_date=movie.xpath('./div[4]/text()')
+            item['plan_date'] = plan_date.extract()[1].strip()
+            print(plan_date.extract()[1].strip())
+            yield item
+
