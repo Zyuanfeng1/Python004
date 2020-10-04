a177753120140d05ca107f4342afe3a4f47faf2d
yuanfeng <929809235@qq.com>
2020-10-04 20:53:08 +0800

    better

--- Week02/spiders/spiders/pipelines.py
+++ Week02/spiders/spiders/pipelines.py
@@ -8,38 +8,31 @@ import pymysql
 
 
 class SpidersPipeline:
-    def process_item(self, item, spider):
-        name = item['name']
-        # item['name']="".join(item['name'].split())
-        category = item['category']
-        plan_date = item['plan_date']
-
-        conn = pymysql.connect(host='localhost',
+    def open_spider(self,spider):
+        self.conn = pymysql.connect(host='localhost',
                                port=3306,
                                user='root',
                                password='',
                                database='test',
                                charset='utf8mb4'
                                )
-
         # 获得cursor游标对象
-        con1 = conn.cursor()
-
+        self.con1 = self.conn.cursor()
+    def process_item(self, item, spider):
+        name = item['name']
+        category = item['category']
+        plan_date = item['plan_date']
+        print(name)
+        print(category)
+        print(plan_date)
         # 操作的行数
-        count = con1.execute('insert into movies values(%s,%s,%s);', (name, category, plan_date))
-        count = con1.execute('commit;')
-
-        # 获得所有查询结果
-        print(con1.fetchall())
-
-        con1.close()
-        conn.close()
-
+        count = self.con1.execute('insert into movies values(%s,%s,%s);', (name, category, plan_date))
+        count = self.con1.execute('commit;')
         # 执行批量插入
         # values = [(id,'testuser'+str(id)) for id in range(4, 21) ]
         # cursor.executemany('INSERT INTO '+ TABLE_NAME +' values(%s,%s)' ,values)
-
-        # output = f'|{name}|\t|{category}|\t|{plan_date}|\n\n'
-        # with open('./maoyan2.csv', 'a', encoding='utf-8') as article:
-        #     article.write(output)
         return item
+    def close_spider(self,spider):
+        self.con1.close()
+        self.conn.close()
+
--- Week02/spiders/spiders/spiders/maoyan.py
+++ Week02/spiders/spiders/spiders/maoyan.py
@@ -18,13 +18,13 @@ class MaoyanSpider(scrapy.Spider):
                 name = movie.xpath('./div[1]/span[@class="name "]/text()')
                 item = SpidersItem()
                 item['name'] = name.extract_first().strip()
-                print(name.extract_first().strip())
+                # print(name.extract_first().strip())
                 category = movie.xpath('./div[2]/text()')
                 item['category'] = category.extract()[1].strip()
-                print(category.extract()[1].strip())
+                # print(category.extract()[1].strip())
                 plan_date = movie.xpath('./div[4]/text()')
                 item['plan_date'] = plan_date.extract()[1].strip()
-                print(plan_date.extract()[1].strip())
+                # print(plan_date.extract()[1].strip())
                 yield item
         except Exception as e:
             print(e)
